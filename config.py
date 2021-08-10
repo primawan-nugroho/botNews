e014a25661568958d885c25367c522e0b38771db
primawan-nugroho <74648388+primawan-nugroho@users.noreply.github.com>
2021-08-10 11:43:20 +0700

    Securing API Key to env

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ def create_api():
     consumer_key = os.getenv("CONSUMER_KEY")
     consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    access_token_secret =os.getenv("ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -21,5 +21,5 @@ def create_api():
     except Exception as e:
         logger.error("Error creating API", exc_info=True)
         raise e
-    logger.info("API created")
+    logger.info("API created")  
     return api
